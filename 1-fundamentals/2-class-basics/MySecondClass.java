3d6e8fa64c161136a1b507845b8c1ffa9f27cc08
zentag <38821230+zentag@users.noreply.github.com>
2026-09-10 14:01:04 -0400

    exercise 2

---
+++ 1-fundamentals/2-class-basics/MySecondClass.java
@@ -0,0 +1,14 @@
+public class MySecondClass {
+        public static void main(String[] args) {
+                MySecondClass instanceOfClass = new MySecondClass();
+
+                System.out.println(instanceOfClass.add(6,7));
+                System.out.?????(instanceOfClass.subtract(6,7));
+        }
+        ????? int add(int x, int y) {
+                return x ????? y;
+        }
+        public int subtract (int x, ????? y) {
+                ????? x - y;
+        };
+}
