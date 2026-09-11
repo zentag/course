d520d4d490cc4288d23812eec12397bacdfabc7d
zentag <38821230+zentag@users.noreply.github.com>
2026-09-10 23:28:05 -0400

    solution for 1.3

--- activities-by-session/1-exploration/3-cooler-intake/src/main/java/frc/robot/RobotContainer.java
+++ activities-by-session/1-exploration/3-cooler-intake/src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,20 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
-        private final Intake intake;
+        private final Intake intake = new Intake();
+        private final CommandXboxController controller = new CommandXboxController(1);
         public RobotContainer() {
-                intake = new Intake();
+                configureBindings();
+        }
+
+        public void configureBindings() {
+                controller.x().whileTrue(Commands.startEnd(intake::intake,intake::stop,intake));
+                controller.b().whileTrue(Commands.startEnd(intake::eject,intake::stop,intake));
         }
         public Intake getIntake() {
                 return intake;
--- activities-by-session/1-exploration/3-cooler-intake/src/main/java/frc/robot/subsystems/Intake.java
+++ activities-by-session/1-exploration/3-cooler-intake/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,9 +1,24 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
 
+        private final TalonFX intakeMotor = new TalonFX(1);
+        public Intake() {}
 
+        public void intake() {
+                intakeMotor.set(1);
+        }
+
+        public void eject() {
+                intakeMotor.set(-1);
+        }
+
+        public void stop() {
+                intakeMotor.set(0);
+        }
 
 }
