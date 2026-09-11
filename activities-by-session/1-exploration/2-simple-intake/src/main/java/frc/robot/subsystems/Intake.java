151c04dcf1b19e35cdb7da8cbb1807e14e094af2
zentag <38821230+zentag@users.noreply.github.com>
2026-09-10 23:04:50 -0400

    solution for 1.2

--- activities-by-session/1-exploration/2-simple-intake/src/main/java/frc/robot/subsystems/Intake.java
+++ activities-by-session/1-exploration/2-simple-intake/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,9 +1,14 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
 
-
+        private final TalonFX intakeMotor = new TalonFX(1);
+        public Intake() {
+                intakeMotor.set(0.75);
+        }
 
 }
