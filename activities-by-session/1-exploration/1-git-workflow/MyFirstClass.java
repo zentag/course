c3bfcaf978cde2d145fbd054dce381d475e04a68
maan13123p-collab <maan13123p@gmail.com>
2026-09-14 22:44:33 +0000

    first commit

--- activities-by-session/1-exploration/1-git-workflow/MyFirstClass.java
+++ activities-by-session/1-exploration/1-git-workflow/MyFirstClass.java
@@ -3,6 +3,7 @@
 
 public class MyFirstClass {
         public static void main(String[] args) {
-                System.out.println("Hello, world");
+                System.out.println("Hello, world!");
         }
 }
+
