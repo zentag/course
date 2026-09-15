e6b32f68f5bba9f842d89f63dcaf5c39f85efe60
ReignRoss <reign.ross@fwcsstudents.org>
2026-09-15 00:09:27 +0000

    9/14/2026

--- activities-by-session/1-exploration/3-cooler-intake/src/main/java/frc/robot/RobotContainer.java
+++ activities-by-session/1-exploration/3-cooler-intake/src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,16 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
         private final Intake intake;
+        private final CommandXboxController controller = new CommandXboxController(1);
         public RobotContainer() {
                 intake = new Intake();
-        }
+        controller.x().whileTrue(Commands.startEnd(intake::intake, inake::stop, intake));
         public Intake getIntake() {
                 return intake;
         }
